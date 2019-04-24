17994cb23f557c99e234d71e23a8ec2d10fb6adb
Oleg Serikov <srkvoa@gmail.com>
2019-04-24 17:35:02 +0300

    added stacked rnn cell to decoder

--- onmt/models/stacked_rnn.py
+++ onmt/models/stacked_rnn.py
@@ -35,7 +35,36 @@ class StackedLSTM(nn.Module):
 
         return input_feed, (h_1, c_1)
 
+    
+class StackedRNN(nn.Module):
 
+    def __init__(self, num_layers, input_size, rnn_size, dropout):
+        super(StackedRNN, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.num_layers = num_layers
+        self.layers = nn.ModuleList()
+
+        for _ in range(num_layers):
+            self.layers.append(nn.RNNCell(input_size, rnn_size))
+            input_size = rnn_size
+
+    def forward(self, input_feed, hidden):
+        h_0, c_0 = hidden
+        h_1, c_1 = [], []
+        for i, layer in enumerate(self.layers):
+            h_1_i, c_1_i = layer(input_feed, (h_0[i], c_0[i]))
+            input_feed = h_1_i
+            if i + 1 != self.num_layers:
+                input_feed = self.dropout(input_feed)
+            h_1 += [h_1_i]
+            c_1 += [c_1_i]
+
+        h_1 = torch.stack(h_1)
+        c_1 = torch.stack(c_1)
+
+        return input_feed, (h_1, c_1)
+
+    
 class StackedGRU(nn.Module):
     """
     Our own implementation of stacked GRU.
